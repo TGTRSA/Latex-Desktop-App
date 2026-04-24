c8b6d85760c7d6483ef052098f6d51a81f8f4d13
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-04-24 13:03:46 +0000

    Migrating the file handling to python instead of cpp for easier use you will not the file_handler.py in src

---
+++ src/file_handler.py
@@ -0,0 +1,53 @@
+import sys
+from pathlib import Path
+import subprocess
+
+EXTENSION   = ".tex"
+FOLDERNAME  = "../tex_files/"
+
+
+
+class File:
+    def __init__(self,name, content):
+        self.directory = Path(f"{FOLDERNAME}")
+        self.path = Path(f"{FOLDERNAME}{name}{EXTENSION}")
+
+        self.content = content
+
+    def validate_dir(self):
+        if not self.directory.is_dir():
+            self.directory.mkdir()
+            return True
+        else:
+            return True
+
+    def create(self):
+        if(self.validate_dir):
+            with open(self.path, 'w') as f:
+                f.write(self.content)
+        else:
+            self.directory.mkdir()
+
+    def rm(self):
+        self.path.unlink()
+
+
+
+
+if __name__ == "__main__":
+    argc = len(sys.argv)
+    if (argc> 3 or argc <3):
+        print(f"Value error: args {argc}")
+        exit(1)
+    else:
+        new_file = File(sys.argv[1], sys.argv[2])
+        print(f"File: {new_file.path}")
+        new_file.create()
+
+        cmd = ["ls", f"{FOLDERNAME}"]
+        cat_cmd = ["cat", f"{new_file.path}"]
+        subprocess.run(cmd)
+        subprocess.run(cat_cmd)
+        delete = input("\nDo you want to delete the file: ")
+        if(delete):
+            new_file.rm()
